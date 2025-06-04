d869de45f170efa13216ef22bc4616c6e8883ecb
Zeeshan-191 <zeeshushaikh191@gmail.com>
2025-06-04 01:49:17 -0700

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,28 @@
+s="INSTAGRAM"
+
+print(s[6:9])
+
+print(len(s))
+
+print(s[0:len(s)])
+
+print(s[0:9:2])
+
+print(s[ : :2])
+
+print(s[3:6:-1])
+
+print(s[9:0:-1])
+
+print(s[ : :-1])
+
+print(s[ : :-2])
+
+#homework
+print(s[-9:-6])
+print(s[ : :-1])
+print(s[7:2:-1])
+print(s[ :4:-1])
+print(s[5: :-1])
+print(s[ :-4:-1])
+print(s[ : :-2])
